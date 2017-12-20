0d92438c435744b36fdbf8a138382529e80f0acd
Guillaume <gari.guillaume@gmail.com>
2017-12-20 17:25:08 +0100

    add script file for LcioToRoot processor

---
+++ script/LcioToRootProcessor.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+class Params :
+	def __init__(self) :
+		self.collectionName = 'HCALEndcap'
+		self.outputFileName = 'analysis.root'
+		self.maxRecordNumber = 0
+
+def launch(a , files) :
+
+	fileList = ''
+	for name in files :
+		fileList += name + ' '
+
+	pid = os.getpid()
+
+	xmlFileName = str(pid) + '.xml'
+	tempOutputFile = str(pid) + '.root'
+
+	xml = '''<marlin>
+ <execute>
+  <processor name="LcioToRootProcessor"/>
+ </execute>
+
+ <global>
+  <parameter name="LCIOInputFiles">''' + fileList + '''</parameter>
+  <parameter name="MaxRecordNumber" value="''' + str(a.maxRecordNumber) + '''"/>
+  <parameter name="SupressCheck" value="false" />
+  <parameter name="Verbosity" options="DEBUG0-4,MESSAGE0-4,WARNING0-4,ERROR0-4,SILENT"> MESSAGE </parameter> 
+ </global>
+
+ <processor name="LcioToRootProcessor" type="LcioToRootProcessor">
+
+  <parameter name="inputHitCollection" type="stringVec" lcioInType="CalorimeterHit">''' + a.collectionName + '''</parameter>
+  <parameter name="inputRelCollection" type="stringVec" lcioInType="LCRelation"> RelationParticleToHit </parameter>
+  <parameter name="inputMCParticleCollection" type="string" lcioInType="MCParticle"> primaryParticles </parameter>
+
+  <parameter name="RootFileName" type="string" >''' + tempOutputFile + '''</parameter>
+
+ </processor>
+
+</marlin>'''
+
+	xmlFile = open(xmlFileName , 'w')
+	xmlFile.write(xml)
+	xmlFile.close()
+
+	os.system('Marlin ' + xmlFileName)
+	os.system('rm ' + xmlFileName)
+	os.system('mv ' + tempOutputFile + ' ' + a.outputFileName)
+
+
+
+if __name__ == '__main__' :
+
+	if len(sys.argv) < 2 :
+		sys.exit('Error : too few arguments')
+
+
+	fileList = [ sys.argv[1] ]
+
+	print 'File List :'
+	print fileList
+
+	os.environ["MARLIN"] = '/home/garillot/ilcsoft/v01-19-03/Marlin/v01-12'
+	os.environ["PATH"] = '/home/garillot/ilcsoft/v01-19-03/Marlin/v01-12/bin' + ':' + os.environ["PATH"]
+	os.environ["MARLIN_DLL"] = '/home/garillot/SDHCALMarlinProcessor/lib/libsdhcalMarlin.so'
+
+	a = Params()
+
+	a.outputFileName = sys.argv[1].replace('.slcio' , '.root')
+
+	launch(a , fileList)
+
