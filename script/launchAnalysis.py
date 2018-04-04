a2480a868c1344e0db06ef3af11f8ab0de3f1bbc
Bing Liu <b.liu@ipnl.in2p3.fr>
2018-04-04 15:03:45 +0200

    add my analysis launcher script

---
+++ script/launchAnalysis.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import os
+import sys 
+
+import AnalysisProcessor as ap
+
+import argparse
+
+if __name__ == '__main__' :
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('runNumber' , help='number of the run' , type=str)
+
+	args = parser.parse_args()
+
+	runNumber = str(args.runNumber) 
+
+
+	#ilcsoft
+	os.environ["MARLIN_DLL"] = '/home/liu/SDHCALMarlinProcessor/lib/libsdhcalMarlin.so'
+
+	inputDir = '/home/liu/files/DATA/TRIVENT/SPS_Sep2017'
+
+	inputFile = 'TDHCAL_' + runNumber
+	fileList = [ inputFile ]
+
+	a = ap.Params()
+	a.collectionName = 'SDHCAL_HIT'
+	a.runNumber = runNumber
+
+	a.outputFileName = runNumber + '.root'
+
+	ap.launch(a , fileList)
+
+
+  	outputDir = inputDir.replace('TRIVENT' , 'Analysis')
+
+  	os.system('mkdir -p ' + outputDir) 
+
+	os.system('mv ' + a.outputFileName + ' ' + outputDir)
